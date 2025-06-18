869e28452aba26b02aed5b47af4d59a00946c076
unknown <mebannongrum333@gmail.com>
2025-06-18 16:51:13 +0530

    refractor failed test cases- country flag and name

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -5,7 +5,7 @@ function CountryCard({country}) {
   return (
    <div className='countryCard'>
         <img src={country.png} alt={country.common} height='100' width='100' />
-        <h1>{country.common}</h1>
+        <h2>{country.common}</h2>
     </div>
   )
 }
