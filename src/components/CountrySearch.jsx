e9482a9ed4f7d714b8b833eb909e5cfc77cfb4c3
unknown <mebannongrum333@gmail.com>
2025-06-18 15:49:25 +0530

    first commit

---
+++ src/components/CountrySearch.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react'
+import style from './CountrySearch.module.css'
+import axios from "axios";
+
+export default function Countries() {
+    const API_ENDPOINT = 'https://countries-search-data-prod-812920491762.asia-south1.run.app/countries';
+    const [countries, setCountries] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filterCountries, setFilterCountries] = useState([]);
+    const [debounceTimeout, setDebounceTimeout] = useState(null);
+    useEffect(() => {
+        getAllCountries();
+    },[]);
+
+    //Effect for searching countries
+    useEffect(() => {
+        if(searchTerm !== '') {
+        const time = setTimeout(() =>{
+            performSearch(searchTerm);
+        },500);
+        setDebounceTimeout(time);
+        } else {
+            setFilterCountries(countries);
+        }
+    },[searchTerm]);
+
+    // Get All countries
+    const getAllCountries = async () => {
+        try {
+         const response = await axios.get(API_ENDPOINT);
+         setCountries(response.data); 
+         setFilterCountries(response.data);  
+        } catch (error) {
+            console.error('Error fetching data:',error);
+        }        
+    }
+
+    //Perform search
+    const performSearch = async (text) => {
+        const c = countries.filter(country => country.common.toLowerCase().includes(text.toLowerCase()));
+        setFilterCountries(c);
+    };
+    
+    //Debounce search
+    const debounceSearch = (event, debounceTimeout) => {
+        if(debounceTimeout) {
+        clearTimeout(debounceTimeout);
+        }
+        setSearchTerm(event.target.value);
+    };
+
+    return (
+        <div>
+            <div className={style.searchbar}>
+                <input type="text" onChange={(e) => debounceSearch(e, debounceTimeout)} placeholder='Search for countries'/>
+            </div>            
+            <div className={style.wrapper}>
+                {
+                        filterCountries.map((country, idx) => (
+                            <div className={style.countryCard} key={country.common}>
+                                <img src={country.png} alt={`Flag of ${country.common}`} height='100' width='100' />
+                                <h5>{country.common}</h5>
+                            </div>
+                        ))
+                } 
+            </div>
+        </div>        
+    )     
+}
