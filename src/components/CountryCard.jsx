298110a984e017c31b2629b135804279c2081fa8
unknown <mebannongrum333@gmail.com>
2025-06-18 16:46:21 +0530

    refractor countryCard failed test cases

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import style from './CountryCard.module.css'
+import'./CountryCard.css'
 
 function CountryCard({country}) {
   return (
-   <div className={style.countryCard}>
+   <div className='countryCard'>
         <img src={country.png} alt={country.common} height='100' width='100' />
         <h1>{country.common}</h1>
     </div>
